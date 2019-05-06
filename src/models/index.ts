77b65e4956480205f983463dda13532a7723526f
Prem Chintalapudi <prem.chintalapudi@gmail.com>
2019-05-06 19:04:51 -0400

    Restarting for V2.0

---
+++ src/models/index.ts
@@ -0,0 +1,12 @@
+import element_defs from './elements';
+
+type ChemicalElement = typeof element_defs[0];
+
+interface Payload {
+    name: string;
+    symbol: string;
+}
+
+class RGroup {
+    constructor(public payload: Payload, public x = 0, public y = 0, public charge = 0) { }
+}
