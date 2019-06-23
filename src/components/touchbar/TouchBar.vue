bc3ad179c8883c408fbce92b48f525b666b54e65
Prem Chintalapudi <prem.chintalapudi@gmail.com>
2019-06-22 22:18:08 -0700

    Touchbar transitions

--- src/components/touchbar/TouchBar.vue
+++ src/components/touchbar/TouchBar.vue
@@ -1,26 +1,69 @@
 <template>
-  <span class="touch-bar">
+  <span
+    class="touch-bar"
+    :style="style"
+    @mouseenter="mouseEnterEvent"
+    @mouseleave="mouseLeaveEvent"
+  >
     <atom-creator-vue :state-machine="stateMachine" @button-click="cascade"></atom-creator-vue>
     <lone-pair-creator-vue :state-machine="stateMachine" @button-click="cascade"></lone-pair-creator-vue>
     <arrow-creator-vue :state-machine="stateMachine" @button-click="cascade"></arrow-creator-vue>
   </span>
 </template>
 <script lang="ts">
-import Vue from "vue";
+import Vue, { PropType } from "vue";
 import AtomCreatorVue from "./AtomCreator.vue";
 import LonePairCreatorVue from "./LonePairCreator.vue";
 import ArrowCreatorVue from "./ArrowCreator.vue";
-import { StateMachine } from "../../state_machine";
+import { StateMachine, State } from "../../state_machine";
+type StyleProperty = [string, string];
 export default Vue.extend({
-  props: { stateMachine: StateMachine },
+  props: { stateMachine: Object as PropType<StateMachine> },
   components: {
     "atom-creator-vue": AtomCreatorVue,
     "lone-pair-creator-vue": LonePairCreatorVue,
     "arrow-creator-vue": ArrowCreatorVue
   },
+  data() {
+    return {
+      transparent: false,
+      timeoutCode: undefined as number | undefined
+    };
+  },
+  computed: {
+    overrideTransparent(): boolean {
+      const sv = this.stateMachine.stateVariables;
+      return (
+        this.stateMachine.state !== State.IDLE ||
+        !(sv.rgroups.length || sv.straightArrows.length)
+      );
+    },
+    style(): string {
+      const styles: StyleProperty[] = [];
+      if (this.transparent && !this.overrideTransparent) {
+        styles.push(["opacity", "0.1"]);
+      }
+      return styles.map(sp => sp.join(":")).join(";");
+    }
+  },
   methods: {
     cascade(payload: { target: string; payload: any }) {
       this.$emit("button-click", payload);
+    },
+    mouseEnterEvent() {
+      this.transparent = false;
+      if (this.timeoutCode !== undefined) window.clearTimeout(this.timeoutCode);
+      this.timeoutCode = undefined;
+    },
+    mouseLeaveEvent() {
+      if (this.timeoutCode === undefined) {
+        this.timeoutCode = window.setTimeout(() => {
+          this.timeoutCode = undefined;
+          if ((this.transparent = !this.overrideTransparent)) {
+            this.mouseLeaveEvent();
+          }
+        }, 1500);
+      }
     }
   }
 });
@@ -32,14 +75,10 @@ export default Vue.extend({
   align-items: center;
   background-color: #00000011;
   max-height: 10%;
-  opacity: 0.1;
   transition-property: opacity;
   transition: opacity 500ms;
 }
-.touch-bar>* {
+.touch-bar > * {
   margin: 5px;
-}
-.touch-bar:hover {
-  opacity: 1;
 }
 </style>
