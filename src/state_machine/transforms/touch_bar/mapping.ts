8efa558cf9bbcd390a2d625b04b4764cb1743348
Prem Chintalapudi <prem.chintalapudi@gmail.com>
2019-07-19 23:20:46 -0700

    Corrected tslint errors

--- src/state_machine/transforms/touch_bar/mapping.ts
+++ src/state_machine/transforms/touch_bar/mapping.ts
@@ -4,5 +4,4 @@ import { State } from "@/state_machine";
 export const mapping: Transform = (stateMachine) => {
     stateMachine.state = State.MAPPING;
     stateMachine.stateVariables.ipos.length = 0;
-    stateMachine.view.viewPort.stopEasing();
 };
--- src/state_machine/transforms/touch_bar/panning.ts
+++ src/state_machine/transforms/touch_bar/panning.ts
@@ -4,5 +4,4 @@ import { State } from "@/state_machine";
 export const panning: Transform = (stateMachine) => {
     stateMachine.state = State.PANNING;
     stateMachine.stateVariables.ipos.length = 0;
-    stateMachine.view.viewPort.stopEasing();
 };
